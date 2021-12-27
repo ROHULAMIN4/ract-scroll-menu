ac6f2e09d7b52799c7c2639417198e7abbe22016
Rohul Amin <rohulamin83716@gmail.com>
2021-12-27 22:10:18 +0600

    react and js scroll menu chnage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+const App = () => {
+  const [li1, setLi1] = useState(false);
+  const [li2, setLi2] = useState(false);
+  const [li3, setLi3] = useState(false);
 
-function App() {
+  useEffect(() => {
+    // Button is displayed after scrolling for 500 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 100 && window.pageYOffset < 299) {
+        setLi1(true);
+        setLi2(false);
+        setLi3(false);
+
+        // alert("hello");
+      } else if (window.pageYOffset > 300 && window.pageYOffset < 599) {
+        setLi2(true);
+        setLi1(false);
+
+        setLi3(false);
+      } else if (window.pageYOffset > 600 && window.pageYOffset < 899) {
+        setLi3(true);
+        setLi1(false);
+        setLi2(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scroll-to-top">
+      <div className="id4">
+        <ul>
+          <li id="in">home</li>
+          <li id="inn">About</li>
+          <li id="innn">Alo</li>
+        </ul>
+      </div>
+      {li1 && (
+        <div className="id4">
+          <ul>
+            <li id="in" className="std">
+              home
+            </li>
+            <li id="inn">About</li>
+            <li id="innn">Alo</li>
+          </ul>
+        </div>
+      )}
+      {li2 && (
+        <div className="id4">
+          <ul>
+            <li id="in">home</li>
+            <li id="inn" className="std">
+              About
+            </li>
+            <li id="innn">Alo</li>
+          </ul>
+        </div>
+      )}
+      {li3 && (
+        <div className="id4">
+          <ul>
+            <li id="in">home</li>
+            <li id="inn">About</li>
+            <li id="innn" className="std">
+              Alo
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
